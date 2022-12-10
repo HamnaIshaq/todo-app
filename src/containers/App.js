825836a6088c202abec81cf924dde0e9080bd807
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-10 19:30:26 +0500

    Delete a single todo item

--- src/containers/App.js
+++ src/containers/App.js
@@ -19,9 +19,14 @@ class App extends Component {
     this.setState({ todoItem: '' });
   }
 
+  onDeleteButtonClick = (e) => {
+    const deleteTodo = e.currentTarget.getAttribute('data-todo');
+    this.setState({ todoList: this.state.todoList.filter(todo => todo !== deleteTodo)})
+  }
+
   render() {
     return(
-      <div>
+      <div className='todo-container'>
         <TodoInput
           inputChange = {this.onInputChange}
           inputValue = {this.state.todoItem}
@@ -29,6 +34,7 @@ class App extends Component {
         /> 
         <TodoItem 
           todos = {this.state.todoList}
+          onDeleteButtonClick={this.onDeleteButtonClick}
         />
       </div>
     );
