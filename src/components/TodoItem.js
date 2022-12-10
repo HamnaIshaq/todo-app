3aec9f7827d45ef5cc6ec6ebcabf2e95f49193e7
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-10 19:30:08 +0500

    Show todo items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import '../assets/style.css';
+import CrossIcon from '../assets/images/icon-cross.svg';
+import CheckIcon from '../assets/images/icon-check.svg';
 
-const TodoItem = ({ todos }) => {
+const TodoItem = ({ todos, onDeleteButtonClick }) => {
   
   const todoList =  todos.map((todo, index) => {
-    return <li key={index}> {todo} <span>X</span></li>
+    return <div key={index} className='todo-item-container'>
+      <li className='todo-item'> 
+        <label htmlFor="todo-completed" className='todo-checkbox-label'>
+          <input type="checkbox" className='todo-checkbox-input'  name="todo-completed" id="todo-completed" />
+          <button type="button" className="btn todo-completed-btn">
+            {/*<img className="todo-completed-img" src={CheckIcon} alt="check" />*/}
+          </button>
+        </label>
+        <p className='todo'>{todo}</p> 
+        
+        <button type='button' className='btn delete-todo-btn' onClick={onDeleteButtonClick} data-todo={todo}>
+          <img className='delete-todo-img' src={CrossIcon} alt="delete" />
+        </button>
+      </li>
+    </div>
   })
 
   return(
-    <div className='todo-container'>
-      <ul>
+    <div>
+      <ul className='todo-list'>
         {todoList}
       </ul>
     </div>
